47d18e03da330fb93ba66efbb71e8f61d3785ed6
Hyejeong Choi <lucyhchoi03@gmail.com>
2025-05-20 11:56:01 -0700

    add class4

---
+++ class04/class04.R
@@ -0,0 +1,10 @@
+#' ---
+#' title: "Intro to R"
+#' author: "Hyejeong Choi"
+#' date: "April 10, 2025"
+#' ---
+# My first R script
+x <- 1:50
+plot(x, sin(x))
+plot(x, sin(x), typ="l", col="green", lwd=3, 
+     xlab="Silly x axis", ylab="Sensible y axis")
